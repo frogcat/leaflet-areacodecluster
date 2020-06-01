35f821ad1a505bc59292ee5778d44355f3b54a48
frogcat <frogcat@users.noreply.github.com>
2020-06-01 16:51:00 +0900

    Fix file structure

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,11 @@
+import fs from 'fs';
 import buble from '@rollup/plugin-buble';
 import json from '@rollup/plugin-json';
 import {
   terser
 } from 'rollup-plugin-terser';
 
-export default [{
+const config = [{
   input: 'src/leaflet-areacodecluster.js',
   output: [{
     file: 'dist/leaflet-areacodecluster.js',
@@ -17,18 +18,39 @@ export default [{
   plugins: [
     buble()
   ]
-}, {
-  input: 'src/leaflet-areacodecluster-japan.js',
-  output: [{
-    file: 'dist/leaflet-areacodecluster-japan.js',
-    format: 'iife'
-  }, {
-    file: 'dist/leaflet-areacodecluster-japan.min.js',
-    format: 'iife',
-    plugins: [terser()]
-  }],
-  plugins: [
-    json(),
-    buble()
-  ]
 }];
+
+fs.readdirSync('config').filter(x => x.match(/\.json$/)).forEach(x => {
+
+  const name = x.replace(".json", "");
+  const src = `src/leaflet-areacodecluster-${name}.js`;
+
+  fs.writeFileSync(src, `import './leaflet-areacodecluster.js';
+
+import areaCodeDefinition from '../config/${x}';
+
+(function(L) {
+  L.areaCodeCluster.${name} = function(markers, options) {
+    return new L.AreaCodeCluster(areaCodeDefinition, markers, options);
+  };
+})(window.L);
+`, "utf-8");
+
+  config.push({
+    input: src,
+    output: [{
+      file: `dist/leaflet-areacodecluster-${name}.js`,
+      format: 'iife'
+    }, {
+      file: `dist/leaflet-areacodecluster-japan.min.js`,
+      format: 'iife',
+      plugins: [terser()]
+    }],
+    plugins: [
+      json(),
+      buble()
+    ]
+  });
+});
+
+export default config;
--- src/leaflet-areacodecluster-japan.js
+++
@@ -1,9 +0,0 @@
-import './leaflet-areacodecluster.js';
-
-import areaCodeDefinition from '../data/japan.json';
-
-(function(L) {
-  L.areaCodeCluster.japan = function(markers, options) {
-    return new L.AreaCodeCluster(areaCodeDefinition, markers, options);
-  };
-})(window.L);
